fe6399309fcb6725eae57aa58c47651ea0997f48
Your Name <you@example.com>
2024-06-02 23:52:40 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// -------typing effect------
+
+  // Initialize Typed.js
+  document.addEventListener('DOMContentLoaded', function() {
+    var typingEffect = new Typed('.typedText', {
+      strings: ["Designer", "Coder", "Developer"],
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 80,
+      backDelay: 2000,
+    });
+  });
+
+
+ 
